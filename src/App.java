b7932cda3e56381405f855e151eaa6ea871a9c4a
Christopher Waschke <chrisdwaschke@gmail.com>
2025-02-26 13:39:04 -0800

    Added test for LinkedList.

--- src/App.java
+++ src/App.java
@@ -1,4 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        List<String> strList = new List<>();
+        strList.add("Test");
+        strList.add("Test2");
+        strList.add("Test4");
+        strList.add("Test8");
+        LinkNode<String> x;
+        for(int i = 0; i<strList.length(); i++){
+            System.out.println(strList.get(i));
+        }
+
+        strList.remove(2);
+
+        for(int i = 0; i<strList.length(); i++){
+            System.out.println(strList.get(i));
+        }
+
     }
 }
