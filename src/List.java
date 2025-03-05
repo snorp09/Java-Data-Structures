7c309b1fa46dc0f2a8977522d42e624dab1094a5
Christopher Waschke <chrisdwaschke@gmail.com>
2025-03-05 10:30:58 -0800

    Moved Linked List test into Class.

--- src/List.java
+++ src/List.java
@@ -72,4 +72,23 @@ public class List<T> {
     public int length(){
         return this.length;
     }
+
+    public static void main(String[] args) throws Exception {
+        List<String> strList = new List<>();
+        strList.add("Test");
+        strList.add("Test2");
+        strList.add("Test4");
+        strList.add("Test8");
+        LinkNode<String> x;
+        for(int i = 0; i<strList.length(); i++){
+            System.out.println(strList.get(i));
+        }
+
+        strList.remove(2);
+
+        for(int i = 0; i<strList.length(); i++){
+            System.out.println(strList.get(i));
+        }
+
+    }
 }
