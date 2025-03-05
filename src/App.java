4d8006033bb514a1be484f837a28304f431ddbaa
Christopher Waschke <chrisdwaschke@gmail.com>
2025-03-05 10:31:31 -0800

    Added Binary Search Tree tests.

--- src/App.java
+++ src/App.java
@@ -1,20 +1,18 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        List<String> strList = new List<>();
-        strList.add("Test");
-        strList.add("Test2");
-        strList.add("Test4");
-        strList.add("Test8");
-        LinkNode<String> x;
-        for(int i = 0; i<strList.length(); i++){
-            System.out.println(strList.get(i));
-        }
-
-        strList.remove(2);
-
-        for(int i = 0; i<strList.length(); i++){
-            System.out.println(strList.get(i));
-        }
-
+    public static void main(String[] args) {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        tree.add(25);
+        tree.add(20);
+        tree.add(10);
+        tree.add(22);
+        tree.add(36);
+        tree.add(30);
+        tree.add(28);
+        tree.add(40);
+        tree.add(12);
+        tree.add(38);
+        tree.add(48);
+        tree.add(5);
+        System.out.println("Gemma.");
     }
 }
