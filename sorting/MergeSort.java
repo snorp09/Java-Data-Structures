b01a98a302b2fbfef1d5e14ceffdf3b33e672546
Christopher Waschke <Chrisdwaschke@gmail.com>
2025-03-14 10:19:12 -0700

    Added basis of merge sort.

---
+++ sorting/MergeSort.java
@@ -0,0 +1,17 @@
+//Citation: "Merge sort in 3 minutes" https://www.youtube.com/watch?v=4VqmGXwpLqc
+
+package sorting;
+
+public class MergeSort {    
+
+    public static <T extends Comparable<T>> void merge(T[] data){
+
+    }
+
+    public static <T extends Comparable<T>> void mergeSort(T[] data) {
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
